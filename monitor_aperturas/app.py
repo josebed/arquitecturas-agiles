94aa0783de38dfe4a9e0022984143f8d4518bbaa
josebed <je.bedoya@uniandes.edu.co>
2022-10-01 21:18:59 -0500

    modifying spoofing

--- monitor_aperturas/app.py
+++ monitor_aperturas/app.py
@@ -1,5 +1,6 @@
 from trace import Trace
 from flask import Flask, request, jsonify
+from flask_jwt_extended import JWTManager
 from sqlalchemy import false
 from modelos import db
 from flask_restful import Api
@@ -9,6 +10,8 @@ import logging
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///monitor_aperturas.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'frase-secreta'
+app.config['PROPAGATE_EXCEPTIONS'] = True
 
 app_context = app.app_context()
 app_context.push()
@@ -19,6 +22,8 @@ api = Api(app)
 
 api.add_resource(AgregarRegla, '/monitor_aperturas/<int:id_usuario>/reglas')
 api.add_resource(VistaRoot, '/')
+
+jwt = JWTManager(app)
 
 if __name__ == '__main__':
     logging.basicConfig(filename='log_monitor_aperturas.log')
