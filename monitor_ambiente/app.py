d8b5d3db74947c7452bd78b38cdfa05904077608
dhuertasm <d.huertasm@uniandes.edu.co>
2022-10-02 17:36:16 -0500

    Primer commit

--- monitor_ambiente/app.py
+++ monitor_ambiente/app.py
@@ -3,9 +3,11 @@ from modelos import db
 from flask_restful import Api
 from vistas import AgregarRegla, VistaRoot
 import time
+import logging
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///monitor_ambiente.db'
+app.config['JWT_SECRET_KEY'] = "clave_secreta"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app_context = app.app_context()
@@ -19,5 +21,5 @@ api.add_resource(AgregarRegla, '/monitor_ambiente/reglas')
 api.add_resource(VistaRoot, '/')
 
 if __name__ == '__main__':
-
+    logging.basicConfig(filename="log_motitor_ambiente.log")
     app.run(debug=True, host='0.0.0.0', port=5010)
